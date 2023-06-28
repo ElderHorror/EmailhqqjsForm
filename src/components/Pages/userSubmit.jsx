d2fabf95215301c9a66a15c7912bb13743c8c42f
ElderHorror <107815490+ElderHorror@users.noreply.github.com>
2023-06-28 22:42:32 +0100

    Update userSubmit.jsx

--- src/components/Pages/userSubmit.jsx
+++ src/components/Pages/userSubmit.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 function UserSubmit() {
   return (
-    <div className="font-['lato'] text-black  flex flex-col items-center">
-      <p className="text-2xl sm:text-3xl font-semibold mb-8 ">
+    <div className="font-['lato'] text-black xs:max-[700px]:w-screen flex flex-col items-center">
+      <p className="text-2xl xs:max-sm:w-[300px] sm:text-3xl font-semibold mb-8 text-center ">
         We have received your application! ✅
       </p>
       <p className="mb-8">We look forward to getting to know you!</p>
-      <div className="flex flex-col items-center w-[440px] sm:w-[580px] lg:w-[1000px]">
+      <div className="flex flex-col items-center w-[300px] sm:w-[580px] lg:w-[1000px]">
         <img
-          className="sm:w-[480px] sm:h-[280px] md:w-[600px] md:h-[340px] md:h-[280px] w-[400px] h-[240px] rounded-md mb-8"
+          className="sm:w-[480px] sm:h-[280px] md:w-[600px] md:h-[340px] md:h-[280px] h-[220px] rounded-md mb-8"
           src="https://perspective.imgix.net/6393336aeb03e806e07f7baf.jpg"
           alt=""
         />
@@ -30,23 +30,23 @@ function UserSubmit() {
   
      
      <p className="text-center text-2xl font-semibold mb-8">Here's what happens next:</p>
-      <div className="flex flex-col  md:items-start md:flex-row   w-[400px] md:w-[700px] lg:w-[800px] sm:w-[500px]">
+      <div className="flex flex-col  md:items-start md:flex-row   w-[300px] md:w-[700px] lg:w-[800px] sm:w-[500px]">
         
         <div className="flex flex-col justify-center items-start w-[600px]">
       
-        <div className="flex flex-col items-start space-y-6 text-[#575B60] mb-10 w-full">
-          <div className="flex flex-row space-x-4">
+        <div className="flex flex-col items-start space-y-6 text-[#575B60] mb-10 w-[400px]">
+          <div className="flex flex-row space-x-4 w-[320px] sm:max-md:w-[500px] md:max-xl:w-[400px]">
             <p className="text-3xl">📃</p>
-            <div className="flex flex-col w-[360px] sm:max-md:w-[500px] md:max-xl:w-[300px]">
+            <div className="flex flex-col w-[300px] sm:max-md:w-[500px] md:max-xl:w-[300px]">
               <p className="font-semibold">We will review your application</p>
               <p className="">
                 We will look at your application and see if we are a good fit
               </p>
             </div>
           </div>
-          <div className=" flex flex-row space-x-3 w-[440px] sm:max-md:w-[500px] md:max-xl:w-[400px]">
+          <div className=" flex flex-row space-x-3 w-[340px] sm:max-md:w-[500px] md:max-xl:w-[400px]">
             <p className="text-3xl">☎️</p>
-            <div className="flex flex-col ">
+            <div className="flex flex-col">
               <p className="font-semibold">Introductory Meeting</p>
               <p>
                 We will call you and get to know each other in a short phone
@@ -56,10 +56,9 @@ function UserSubmit() {
           </div>
         </div>
        
-        
-      </div>
       
-          <div className="space-x-2 flex flex-row mb-8 md:space-x-3 text-[#575B60]">
+      </div>
+         <div className="space-x-2 flex flex-row mb-8 md:space-x-3 text-[#575B60]">
             <p className="text-3xl ">✅</p>
             <div className="flex flex-col ">
               <p className="font-semibold">Individual Job Offer</p>
@@ -70,11 +69,13 @@ function UserSubmit() {
               </p>
             </div>
           </div>
+        
         </div>
-      <button className="w-full sm:w-[400px] h-[50px] rounded-md bg-[#222F65] text-white text-bold text-xl">
+      
+           <button className="w-full xs:max-sm:w-[300px] sm:w-[400px] h-[50px] rounded-md bg-[#222F65] text-white text-bold text-xl">
           To Our Career Section
         </button>
-        <p className="mt-6 underline text-[#222F65]">Recruiting by OD Solutions GmbH</p>
+        <p className="my-6 underline text-center text-[#222F65]">Recruiting by OD Solutions GmbH</p>
     </div>
   );
 }
