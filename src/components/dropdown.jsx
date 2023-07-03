e0cae1e51fbee1910291fb710616cfbc35c874ea
ElderHorror <107815490+ElderHorror@users.noreply.github.com>
2023-07-03 19:27:37 +0100

    Update dropdown.jsx

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,18 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { FaExclamationTriangle } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaExclamationTriangle, FaChevronDown } from 'react-icons/fa';
 
 function Dropdown(props) {
   const [isComplete, setIsComplete] = useState(false);
   const [selectedOption, setSelectedOption] = useState(null);
   const [isExpanded, setIsExpanded] = useState(false);
+  const [hasSelectedOption, setHasSelectedOption] = useState(false);
   const options = props.options || [];
-
 
   const handleOptionClick = (option) => {
     setSelectedOption(option);
     setIsExpanded(false);
+    setIsComplete(true);
+    setHasSelectedOption(true);
 
-    // Update the specific dropdown's value in formData
     props.setFormData((prevFormData) => {
       return {
         options: {
@@ -24,85 +25,54 @@ function Dropdown(props) {
             },
           ],
         },
-        jobOptions: prevFormData.jobOptions, // Preserve the existing jobOptions value
+        jobOptions: prevFormData.jobOptions,
       };
     });
-
-    setIsComplete(true);
   };
 
   const handleLabelClick = () => {
     setIsExpanded(!isExpanded);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
-
   return (
-    <form action="" onSubmit={handleSubmit}>
-      <div className={`${props.className} ${isComplete ? '' : 'border-yellow-500'}`}>
-        {selectedOption ? (
-          <div className="label pl-4" onClick={handleLabelClick}>
-            {selectedOption}
-          </div>
-        ) : (
-          <div className="label pl-4 flex flex-row justify-between" onClick={handleLabelClick}>
-            {props.title}
-            {!isComplete && (
+    <div className={`${props.className} ${isComplete ? '' : 'border-yellow-500'}`}>
+      <div className="label pl-2 flex flex-row justify-between" onClick={handleLabelClick}>
+        <div className="flex flex-row items-center">
+          <div className="w-4 mr-6 text-2xl">{props.emote}</div>
+          {selectedOption || props.title}
+        </div>
+        {!isComplete && (
           <div className="flex items-center justify-end pr-4 text-yellow-500">
+            <div className="flex items-center justify-end pr-4 text-black">
+              <FaChevronDown />
+            </div>
             <FaExclamationTriangle className="mr-1" />
           </div>
         )}
-          </div>
-        )}
-        {isExpanded && (
-          <ul className="text-center mt-4">
-            {options.map((option, index) => (
-              <li
-                key={index}
-                className={props.ulClassName}
-                onClick={() => handleOptionClick(option)}
-              >
-                {option}
-              </li>
-            ))}
-             
-          </ul>
-        )}
-       
       </div>
-    </form>
+      {isExpanded && (
+        <>
+          <div className="xs:max-md:fixed xs:max-md:inset-0 xs:max-md:bg-gray-800 xs:max-md:bg-opacity-50 xs:max-md:z-40"></div>
+          <div className="xs:max-md:fixed xs:max-md:inset-x-0 xs:max-md:bottom-0 xs:max-md:z-50">
+            <div className="bg-white rounded-t-lg p-4 xs:max-md:w-full xs:max-md:mx-auto">
+              <div className="md:hidden text-xl font-bold mb-2">{props.title}</div>
+              <ul className="text-center">
+                {options.map((option, index) => (
+                  <li
+                    key={index}
+                    className={props.ulClassName}
+                    onClick={() => handleOptionClick(option)}
+                  >
+                    {option}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
 export default Dropdown;
-
-
-// import React from 'react'
-
-// function Dropdown(props) {
-//   const [value, setValue] = React.useState('fruit')
-//   const experience = [
-//     {label: 'Current Occupation',
-//     options: ['Tax Clerk', 'Tax Specialists', 'Payroll Accountant', 'Financial Accountant', 'Accountant', 'Others in the field of taxation']
-//     }
-  
-//   ]
-  
-//   function handleChange(event) {
-//     setValue(event.target.value)
-//   }
-//   return(
-//     <div>
-//       <label htmlFor="">Current Occupation
-//         <select name="" id="" value={value} onChange={handleChange}>
-//         {experience.map((exp) => (<option>{exp.options}</option>))}
-//       </select>
-//       </label>
-    
-//     </div>
-//   )
-// }
-
-// export default Dropdown;
